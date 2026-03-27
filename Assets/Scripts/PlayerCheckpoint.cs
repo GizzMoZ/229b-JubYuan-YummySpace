5751584826d682d71aea9448cda15efc362c1ec0
GizzMoZ <MickAriya28@gmail.com>
2026-03-27 12:06:14 +0700

    Add more Asset and UI

--- Assets/Scripts/PlayerCheckpoint.cs
+++ Assets/Scripts/PlayerCheckpoint.cs
@@ -1,36 +1,67 @@
 ﻿using UnityEngine;
+using UnityEngine.SceneManagement;
 
 public class PlayerCheckpoint : MonoBehaviour
 {
     public Transform currentCheckpoint;
     public float deathY = -10f;
 
+    private bool isDead = false;
+    private Rigidbody rb;
+
     void Start()
     {
-        // Default = first spawn
+        rb = GetComponent<Rigidbody>();
         currentCheckpoint = GameManager.instance.spawnPoint;
     }
 
     void Update()
     {
+        // 💀 Detect fall
+        if (!isDead && transform.position.y < deathY)
+        {
+            Die();
+        }
+
+        // 🔁 R works ALWAYS
         if (Input.GetKeyDown(KeyCode.R))
         {
             Respawn();
         }
 
-        if (Input.GetKeyDown(KeyCode.R))
+        // ❌ Q only when dead
+        if (isDead && Input.GetKeyDown(KeyCode.Q))
         {
-            Respawn();
-        }
-
-        if (transform.position.y < deathY)
-        {
-            Respawn();
+            QuitGame();
         }
     }
 
-    public void Respawn()
+    void Die()
+    {
+        isDead = true;
+
+        rb.linearVelocity = Vector3.zero;
+        rb.isKinematic = true;
+
+        Debug.Log("You Died! Press R to Respawn or Q to Quit");
+    }
+
+    void Respawn()
     {
         GameManager.instance.SpawnPlayer(currentCheckpoint);
+
+        isDead = false;
+        rb.isKinematic = false;
+    }
+
+    void QuitGame()
+    {
+        Debug.Log("Quit Game");
+
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
     }
 }

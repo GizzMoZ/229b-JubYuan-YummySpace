f620d98ed1a1234f7447e8ceec9a1f32a96310fc
GizzMoZ <MickAriya28@gmail.com>
2026-03-27 14:12:03 +0700

    Add Main Menu

---
+++ Assets/Scripts/MainMenu.cs
@@ -0,0 +1,21 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class MainMenu : MonoBehaviour
+{
+    public void PlayGame()
+    {
+        SceneManager.LoadScene("Level_1");
+    }
+
+    public void QuitGame()
+    {
+        Debug.Log("Quit Game");
+
+        Application.Quit();
+
+#if UNITY_EDITOR
+        UnityEditor.EditorApplication.isPlaying = false;
+#endif
+    }
+}
